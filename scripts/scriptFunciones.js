af51c62479be92e1a6d817c814e29268a1ce1e2e
Luis García <luisgc1@gmail.com>
2024-02-23 16:09:06 +0100

    trabajamos en las clases.js añadiendo las funciones

--- scripts/scriptFunciones.js
+++ scripts/scriptFunciones.js
@@ -1,16 +1,30 @@
-import {lista} from "./scriptMain.js"
+import { lista } from "./scriptMain.js"               /*importamos la lista y el array tareas*/
 import { tareas } from "./scriptMain.js"
 
-export function addTask{
+export function addTask() {                           /*creamos y exportamos las funciones addTask y cleanAll*/
+
+    const a = document.createAttribute("type")
+    a.value = "text"
+
+    let n = localStorage.numTarea
+
+    const caja = document.createElement("input")
+    caja.setAttributeNode(a)
+    caja.value = tareas[n]
+    n++
+    localStorage.setItem("numTarea", n)
+    const item = document.createElement("li")
+    item.appendChild(caja)
+    lista = document.getElementById("tasklist")
+    lista.appendChild(item)
 
 }
 
+export function cleanAll() {
 
+    const itemstareas=document.querySelectorAll("li")
+    for(let i=0;i<itemstareas.length;i++){
+        lista.removeChild(itemsTareas([i]))
+    };
 
-
-
-
-
-export function cleanAll{
-    
 }
